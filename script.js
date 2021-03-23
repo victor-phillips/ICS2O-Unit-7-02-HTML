ca9a781c975cc46535840ec0c603b1637aa3d360
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-23 14:20:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// setip default values
+uval = 0
+// event listener
+document.getElementById('button').addEventListener('click', func1)
+//detector
+function func1 () {
+  uval = document.getElementById('input').value
+  uval = parseInt(uval)
+  if (uval > -1) {
+    document.getElementById('text').innerHTML ='positive'
+  } else {
+    document.getElementById('text').innerHTML ='negative'
+  }
+}
